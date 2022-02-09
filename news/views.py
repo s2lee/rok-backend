379435724f770f7a958a8e736a25227fdc5e9874
s2lee <therok.manager@gmail.com>
2022-02-09 18:36:59 +0900

    remove itemview in views

--- news/views.py
+++ news/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from .serializers import ArticleSerializer, AllArticleSerializer, CommentSerializer
 from .models import Article, Comment
-
-
-# from users.models import Item
 
 
 class AllArticleViewSet(viewsets.ModelViewSet):
@@ -33,33 +27,3 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-# 받는거 따로 사용하는거 따로 분리
-class ItemApiView(APIView):
-    def post(self, request, pk, item_type):
-        user = request.user
-        article = get_object_or_404(Article, pk=pk)
-        # item = Item.objects.get(user=user)
-        success = True
-        print(user)
-        print(item_type)
-        def use_item():
-            nonlocal success
-            _article = getattr(article, item_type)
-            print(_article)
-            # _item = getattr(item, item_type)
-            if _article.filter(id=user.id).exists():
-                _article.remove(user)
-            else:
-                _article.add(user)
-                # if _item >= 1:
-                #     _article.add(user)
-                #     _item -= 1
-                #     setattr(item, item_type, _item)
-                #     item.save()
-                # else:
-                #     success = False
-
-        use_item()
-        message = f'{item_type} 성공적으로 사용' if success else f'{item_type} 부족'
-        return Response({"message": message})
