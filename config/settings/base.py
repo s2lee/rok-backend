6c6e0097a9aeda9a7edf7b6ebb00688465d8e435
s2lee <therok.manager@gmail.com>
2022-02-11 18:41:35 +0900

    settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,15 +153,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 추가
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
