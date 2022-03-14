42ad18fb1b7dbca77b7da33b5a0b019fc12f6324
s2lee <therok.manager@gmail.com>
2022-03-14 15:32:10 +0900

    Fix ArticleActionView

--- news/views.py
+++ news/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status, generics, mixins
-from rest_framework.views import APIView
+from rest_framework import viewsets, status
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, GenericAPIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet
+
 from .serializers import *
 from .models import Article, Comment, Category
 
@@ -35,30 +34,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def action_button(request, pk, action_type):
-    article = get_object_or_404(Article, pk=pk)
-    article_action = getattr(article, action_type)
-    user = request.user
-    response = Response(
-        {'detail': f'{action_type} 성공적으로 사용.'},
-        status=status.HTTP_200_OK,
-    )
-    if article.author != user:
-        if article_action.filter(id=user.id).exists():
-            article_action.remove(user)
-            response.data = {'detail': f'{action_type} 사용을 취소합니다.'}
-        else:
-            article_action.add(user)
-    else:
-        response.data = {'detail': '기사 작성자는 사용할 수 없습니다.'}
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-    response.data['total_action_count'] = article_action.count()
-    return response
-
-
-class ArticleListCreateAPIView(generics.ListCreateAPIView):
+class ArticleListCreateAPIView(ListCreateAPIView):
     def get_queryset(self):
         return Article.objects.filter(category__name=self.kwargs.get('category'))
 
@@ -83,26 +59,18 @@ class ArticleListCreateAPIView(generics.ListCreateAPIView):
         serializer.save(author=self.request.user, category=category)
 
 
-class ArticleDetailAPIView(generics.RetrieveAPIView):
+class ArticleDetailAPIView(RetrieveAPIView):
     serializer_class = ArticleDetailSerializer
     queryset = Article.objects.select_related('author').prefetch_related(
             'spear', 'shield').all()
 
 
-class ArticleActionView(APIView):
-    permission_classes = IsAuthenticated
+class ArticleActionView(GenericAPIView):
+    permission_classes = (IsAuthenticated, )
 
     def post(self, request, pk, action_type):
-        return self.action()
-
-    def get_action_type(self, pk, action_type):
-        article = self.get_queryset()
+        article = get_object_or_404(Article, pk=pk)
         article_action = getattr(article, action_type)
-
-        return article_action
-
-    def action(self):
-        article_action = self.get_action_type()
         user = request.user
         response = Response(
             {'detail': f'{action_type} 성공적으로 사용.'},
@@ -119,11 +87,4 @@ class ArticleActionView(APIView):
             response.status_code = status.HTTP_401_UNAUTHORIZED
         response.data['total_action_count'] = article_action.count()
         return response
-        pass
-
-    def get_queryset(self, pk):
-        article = get_object_or_404(Article, pk=pk)
-        return article
-
-
 
