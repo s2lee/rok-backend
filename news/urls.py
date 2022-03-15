a2eb80da268368685f53e4ed917b74858e08813f
s2lee <therok.manager@gmail.com>
2022-03-15 20:16:49 +0900

    Remove CommentViewSet urls

--- news/urls.py
+++ news/urls.py
@@ -1,20 +1,11 @@
 from .views import *
-from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
 
-router = DefaultRouter()
-
-comment_list = CommentViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('home/', HomeArticleListView.as_view()),
     path('<str:category>/', ArticleListCreateAPIView.as_view()),
     path('<str:category>/<int:pk>/', ArticleDetailAPIView.as_view()),
     path('<int:pk>/<str:action_type>', ArticleActionView.as_view()),
-    path('<int:article_id>/comments/', comment_list, name='comment-list'),
+    path('<int:article_id>/comments/', CommentListCreateAPIView.as_view())
 ]
