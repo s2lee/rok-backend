57b14f9cd38401791722811f8e43b328a3105600
s2lee <therok.manager@gmail.com>
2022-03-26 08:52:07 +0900

    Add test_comment

--- news/tests.py
+++ news/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Article, Category
+from .models import Article, Category, Comment
 
 
 User = get_user_model()
@@ -15,14 +15,17 @@ class NewsAPITests(APITestCase):
                                              password='password123',
                                              nickname='test_nickname')
         self.category = Category.objects.create(name='art')
-        self.article = Article.objects.create(title='fractal',
+        self.article = Article.objects.create(title='Fractal is wonderful',
                                               category=self.category,
-                                              contents='fractal is wonderful',
+                                              contents='Fractal is ...',
                                               author=self.user)
         self.article_count = Article.objects.all().count()
+        self.comment = Comment.objects.create(article=self.article,
+                                              author=self.user,
+                                              contents='test comment')
 
     def test_article_list(self):
-        response = self.client.get('/art/', format='json')
+        response = self.client.get('/art/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_article_create(self):
@@ -39,3 +42,24 @@ class NewsAPITests(APITestCase):
 
         new_article = Article.objects.latest('id')
         self.assertEqual(new_article.title, data['title'])
+
+    def test_article_detail(self):
+        response = self.client.get('/art/{0}/'.format(self.article.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Fractal is wonderful')
+
+    def test_get_comment(self):
+        response = self.client.get('/{0}/comments/'.format(self.article.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_comment(self):
+        data = {
+            'article': self.article.id,
+            'contents': 'How calculus makes the world smarter',
+            'author': self.user,
+            'parent': self.comment.id
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post('/{0}/comments/'.format(self.article.id), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
