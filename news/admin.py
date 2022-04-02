cb1d27147eb74aae4846abd579a66f8b860b6135
s2lee <therok.manager@gmail.com>
2022-04-03 08:27:16 +0900

    Add is_news in news.admin

--- news/admin.py
+++ news/admin.py
@@ -9,7 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'contents', 'date_posted', 'author', 'image']
+    list_display = ['id', 'title', 'category', 'contents', 'date_posted', 'author', 'image', 'is_news']
 
 
 @admin.register(Comment)
