ddacc067d037a19233a821b5aca32123418bf866
s2lee <therok.manager@gmail.com>
2022-04-05 09:34:50 +0900

    Fix CategoryAdmin

--- news/admin.py
+++ news/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['id', 'name']
 
 
 @admin.register(Article)
