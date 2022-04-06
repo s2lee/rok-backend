50bfc1b441360e143910037f7b20fac97dca8a6e
s2lee <therok.manager@gmail.com>
2022-04-06 20:22:32 +0900

    Refactor ArticleVoteView urls

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('home/', HomeArticleListView.as_view()),
     path('<str:category>/', ArticleListCreateAPIView.as_view()),
     path('<str:category>/<int:pk>/', ArticleDetailAPIView.as_view()),
-    path('<int:pk>/<str:action_type>', ArticleActionView.as_view()),
+    path('<int:article_id>/vote/<str:choice>/', ArticleVoteView.as_view()),
     path('<int:article_id>/comments/', CommentListCreateAPIView.as_view()),
     re_path(r'^news/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/', SearchNewsByDate.as_view()),
 ]
