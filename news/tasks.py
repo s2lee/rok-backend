29116f594ffa2fd4891bc1a8239aaedf58f3d2ee
s2lee <therok.manager@gmail.com>
2022-04-07 10:33:52 +0900

    Fix choose_article_every_midnight from objects to objects_sorted_by_voted in news.tasks

--- news/tasks.py
+++ news/tasks.py
@@ -12,10 +12,8 @@ def choose_article_every_midnight():
     try:
         categories = Category.objects.all()
         for category in categories:
-            articles = Article.objects.filter(
-                category=category, date_posted__date=date.today()).annotate(
-                total_votes=Count('spear', distinct=True) - Count('shield', distinct=True)).order_by(
-                '-total_votes')[:3]
+            articles = Article.objects_sorted_by_vote.filter(
+                category=category, date_posted__date=date.today())[:3]
             for article in articles:
                 article.is_news = True
                 article.save()
