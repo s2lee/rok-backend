04bb438f18db0266ac4b14bd231b69de2ca3515b
s2lee <therok.manager@gmail.com>
2022-04-19 19:02:04 +0900

    Fix from get_user_model to settings.AUTH_USER_MODEL

--- news/models.py
+++ news/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.db.models import Count
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
-User = get_user_model()
+User = settings.AUTH_USER_MODEL
 
 
 class ArticleVoteManager(models.Manager):
