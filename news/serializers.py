8aac8e6e94a3a9d3d39b42df243abd64302213c6
s2lee <therok.manager@gmail.com>
2022-05-09 15:25:47 +0900

    refactor

--- news/serializers.py
+++ news/serializers.py
@@ -5,35 +5,20 @@ from .models import Article, Comment
 class CommonFieldMixin(serializers.Serializer):
     nickname = serializers.ReadOnlyField(source='author.nickname')
     category_name = serializers.ReadOnlyField(source='category.name')
-    comments_count = serializers.SerializerMethodField()
-
-    def get_comments_count(self, obj):
-        return obj.comment.count()
-
-
-class HomeTopArticleSerializer(CommonFieldMixin, serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('id', 'category_name', 'title', 'contents',
-                  'image')
 
 
 class HomeArticleSerializer(CommonFieldMixin, serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('id', 'category_name', 'title', 'nickname')
-
-
-class ArticleSectionSerializer(CommonFieldMixin, serializers.ModelSerializer):
-    total_votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
-        fields = ('id', 'title', 'contents', 'date_posted',
-                  'nickname', 'image', 'comments_count', 'total_votes')
+        fields = ('id', 'category_name', 'title', 'contents', 'image')
 
-    def get_total_votes(self, obj):
-        return obj.spear.count() - obj.shield.count()
+
+class ArticleSectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'contents', 'date_posted', 'image')
 
 
 class ArticleCreateSerializer(serializers.ModelSerializer):
@@ -43,30 +28,26 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
 
 
 class ArticleDetailSerializer(CommonFieldMixin, serializers.ModelSerializer):
-    spear_count = serializers.SerializerMethodField()
-    shield_count = serializers.SerializerMethodField()
+    num_spear = serializers.IntegerField(source='get_total_spear')
+    num_shield = serializers.IntegerField(source='get_total_shield')
     date_posted = serializers.DateTimeField(format="%Y.%m.%d %H:%M")
 
     class Meta:
         model = Article
         fields = ('id', 'title', 'contents', 'nickname', 'author',
-                  'date_posted', 'spear', 'shield', 'image', 'spear_count',
-                  'shield_count', 'comments_count')
-
-    def get_spear_count(self, obj):
-        return obj.spear.count()
-
-    def get_shield_count(self, obj):
-        return obj.shield.count()
+                  'date_posted', 'spear', 'shield', 'image', 'num_spear',
+                  'num_shield')
 
 
 class CommentSerializer(CommonFieldMixin, serializers.ModelSerializer):
     reply = serializers.SerializerMethodField()
-    date_created = serializers.DateTimeField(format="%Y.%m.%d %H:%M", read_only=True)
+    date_created = serializers.DateTimeField(
+        format="%Y.%m.%d %H:%M", read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('id', 'article', 'nickname', 'contents', 'date_created', 'parent', 'reply')
+        fields = ('id', 'article', 'nickname', 'contents',
+                  'date_created', 'parent', 'reply')
 
     def get_reply(self, instance):
         serializer = self.__class__(instance.reply, many=True)
@@ -77,4 +58,4 @@ class CommentSerializer(CommonFieldMixin, serializers.ModelSerializer):
 class SearchNewsByDateSerializer(CommonFieldMixin, serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('id', 'category_name', 'title', 'contents', 'image', 'comments_count')
+        fields = ('id', 'category_name', 'title', 'contents', 'image')
